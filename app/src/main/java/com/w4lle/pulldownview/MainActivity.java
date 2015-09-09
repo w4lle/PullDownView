d4f6653fec4a158f6e55ef3fa4ccbe6451a35d15
wanglinglong <564958390@qq.com>
2015-09-09 21:27:57 +0800

    first add

---
+++ app/src/main/java/com/w4lle/pulldownview/MainActivity.java
@@ -0,0 +1,56 @@
+package com.w4lle.pulldownview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.text1).setOnClickListener(this);
+        findViewById(R.id.text2).setOnClickListener(this);
+        findViewById(R.id.text3).setOnClickListener(this);
+        findViewById(R.id.text4).setOnClickListener(this);
+        findViewById(R.id.text5).setOnClickListener(this);
+        findViewById(R.id.text6).setOnClickListener(this);
+        findViewById(R.id.text7).setOnClickListener(this);
+        findViewById(R.id.text8).setOnClickListener(this);
+        findViewById(R.id.text9).setOnClickListener(this);
+        findViewById(R.id.text10).setOnClickListener(this);
+        findViewById(R.id.text11).setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Log.d("onClick", ((TextView) view).getText().toString());
+    }
+}
---
+++ app/src/main/java/com/w4lle/pulldownview/PullDownView.java
@@ -0,0 +1,182 @@
+package com.w4lle.pulldownview;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ScrollView;
+
+/**
+ * Created by w4lle on 15-9-9.
+ * Copyright (c) 2015 Boohee, Inc. All rights reserved.
+ */
+
+
+public class PullDownView extends ScrollView {
+
+    private int touchSlop;
+    private int screenHeight;
+    //布局的父布局，ScrollView内部只能有一个根ViewGroup，就是此View
+    private ViewGroup parentView;
+    //头部view
+    private View topView;
+
+    private int topViewHeight;
+
+    //显示的内容View
+    private View containerView;
+
+    private float origY;
+
+    private float distanceY;
+
+    private boolean isShowTopView;
+
+    private int mCurrentOffset = 0;//当前右侧滚条顶点的偏移量。ScrollView右侧是有滚动条的，当下拉时，
+    //滚动条向上滑，当向下滑动时，滚动条向下滑动。
+
+    private boolean isonce;
+
+    private MarginLayoutParams layoutParams;
+
+    private int oldTopMargin;
+    //是否是一次连续的MOVE，默认为false
+    private boolean isTouchOne;
+
+    //是否是在最顶部
+    private boolean isTop;
+
+    private ValueAnimator valueAnimator;
+
+    public PullDownView(Context context) {
+        this(context, null);
+    }
+
+    public PullDownView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public PullDownView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics metrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(metrics);
+        screenHeight = metrics.heightPixels;
+        topViewHeight = screenHeight;
+        touchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (!isonce) {
+            parentView = (ViewGroup) this.getChildAt(0);
+            topView = parentView.getChildAt(0);
+            containerView = parentView.getChildAt(1);
+            topView.getLayoutParams().height = topViewHeight;
+            isonce = true;
+
+            layoutParams = (MarginLayoutParams) containerView.getLayoutParams();
+            oldTopMargin = layoutParams.topMargin;
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (!isTouchOne) {
+                    origY = ev.getRawY();
+                    isTouchOne = true;
+                    isTop = getScrollY() == 0 ? true : false;
+                }
+                distanceY = ev.getRawY() - origY;
+                if (distanceY < touchSlop && isShowTopView) {
+                    return true;
+                }
+                if (distanceY < touchSlop) {
+                    return super.onTouchEvent(ev);
+                }
+                if (isTop && distanceY > 0 && !isShowTopView) {
+                    pullDown((int) distanceY / 2);
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                if (isTop && distanceY > containerView.getHeight() / 4 && !isShowTopView) {
+                    showTopView(layoutParams.topMargin, screenHeight);
+                } else if (distanceY > 0 && distanceY < containerView.getHeight() / 4 && !isShowTopView) {
+                    hideTopViewAndShowContainer();
+                }else if (distanceY < 0 && isShowTopView) {
+                    hideTopViewAndShowContainer();
+                }
+                isTouchOne = false;
+                break;
+        }
+        if (isShowTopView) {
+            return true;
+        }
+        return super.onTouchEvent(ev);
+    }
+
+    private void hideTopViewAndShowContainer() {
+        showTopView(layoutParams.topMargin, oldTopMargin);
+        isShowTopView = false;
+    }
+
+    private void pullDown(int distance) {
+        layoutParams.topMargin = distance + oldTopMargin;
+        containerView.setLayoutParams(layoutParams);
+    }
+
+    private void showTopView(int start, int end) {
+        valueAnimator = ValueAnimator.ofInt(start, end);
+        valueAnimator.setDuration(300);
+        valueAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        valueAnimator.start();
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                layoutParams.topMargin = (int) valueAnimator.getAnimatedValue();
+                containerView.setLayoutParams(layoutParams);
+            }
+        });
+        valueAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                post(new Runnable() {
+                    @Override
+                    public void run() {
+//                        scrollTo(0, 0);
+                    }
+                });
+            }
+        });
+        isShowTopView = true;
+    }
+
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        super.onScrollChanged(l, t, oldl, oldt);
+        mCurrentOffset = t;//右边滑动标签相对于顶端的偏移量
+        if (t <= topViewHeight && t >= 0 && !isShowTopView) {
+            topView.setTranslationY(t / 2);//使得TopView滑动的速度小于滚轮滚动的速度
+        }
+    }
+}
